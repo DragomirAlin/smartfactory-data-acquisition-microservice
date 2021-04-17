d04ed1233a3a3c39715588d69b754f3308eedb6c
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-04-17 17:43:32 +0300

    add application default

--- src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
+++ src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package ro.dragomiralin.data.acquisition.security;
 
+import feign.RequestInterceptor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
@@ -34,5 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).build();
         jwtDecoder.setClaimSetConverter(new OrganizationSubClaimAdapter());
         return jwtDecoder;
+    }
+
+    @Bean
+    public RequestInterceptor authRequestInterceptor() {
+        return requestTemplate -> {
+            var jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            requestTemplate.header("Authorization", String.format("Bearer %s", jwt.getTokenValue()));
+        };
     }
 }
