9c418dae0b190be206a52f921e752f46920aa797
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-30 14:47:43 +0300

    change configure

--- src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
+++ src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import feign.RequestInterceptor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,21 +16,17 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    protected void configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/actuator/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/me", "/api/foos/**")
-                .hasAuthority("SCOPE_read")
-                .antMatchers(HttpMethod.POST, "/api/foos")
-                .hasAuthority("SCOPE_write")
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
-    }// @formatter:on
+    }
 
     @Bean
     JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
