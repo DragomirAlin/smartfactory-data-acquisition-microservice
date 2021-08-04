d2e9813468f9bf8f772c4a82999740845513ccce
dragomiralin <dragomirdanielalin@yahoo.com>
2021-08-04 11:31:34 +0300

    update dev config

--- src/main/java/ro/dragomiralin/data/acquisition/controller/AcquisitionController.java
+++ src/main/java/ro/dragomiralin/data/acquisition/controller/AcquisitionController.java
@@ -5,9 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import ro.dragomiralin.data.acquisition.common.Data;
 import ro.dragomiralin.data.acquisition.model.Message;
+import ro.dragomiralin.data.acquisition.service.AcquisitionService;
 import ro.dragomiralin.data.acquisition.service.PublishService;
 import ro.dragomiralin.data.acquisition.service.SubscribeService;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,6 +20,7 @@ import ro.dragomiralin.data.acquisition.service.SubscribeService;
 public class AcquisitionController {
     private final PublishService publishService;
     private final SubscribeService subscribeService;
+    private final AcquisitionService acquisitionService;
 
     @GetMapping
     public String test(@AuthenticationPrincipal Jwt principal) {
@@ -36,5 +41,15 @@ public class AcquisitionController {
     public void subscribe(@AuthenticationPrincipal Jwt principal, @RequestParam String topic) {
         String userId = principal.getClaimAsString("sub");
         subscribeService.subscribe(userId, topic);
+    }
+
+    @GetMapping("/data")
+    public List<Data> allData(@AuthenticationPrincipal Jwt principal) {
+        return acquisitionService.getAll();
+    }
+
+    @GetMapping("/data/{topic}")
+    public List<Data> getDataByTopic(@AuthenticationPrincipal Jwt principal, @PathVariable String topic) {
+        return acquisitionService.getDataByTopic(topic);
     }
 }
