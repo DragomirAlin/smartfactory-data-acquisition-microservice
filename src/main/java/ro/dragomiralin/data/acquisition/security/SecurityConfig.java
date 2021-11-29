456eeb27d4f3b394d3190658cb76b20102311bbe
dragomiralin <dragomirdanielalin@yahoo.com>
2021-11-29 22:03:31 +0200

    fix config

--- src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
+++ src/main/java/ro/dragomiralin/data/acquisition/security/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
